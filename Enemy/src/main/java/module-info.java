ad22af1a5bebfa5d3f7d0e317eb9813b3566a2d3
Bjornestad <frederikbjornestad@gmail.com>
2024-04-09 14:06:05 +0200

    jar pom change

--- Enemy/src/main/java/module-info.java
+++ Enemy/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module Enemy {
     requires Common;
-    requires FXGL;
+    //requires FXGL;
     requires com.almasb.fxgl.entity;
     exports enemy.data;
-    requires Map;
+    //requires Map;
 }
