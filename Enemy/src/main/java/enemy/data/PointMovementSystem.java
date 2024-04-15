0dec83234d382a6e3737c4c22e18d889fde63c03
Yalcin Acili <yalcinacili@gmail.com>
2024-04-15 13:34:37 +0200

    polylines

--- Enemy/src/main/java/enemy/data/PointMovementSystem.java
+++ Enemy/src/main/java/enemy/data/PointMovementSystem.java
@@ -7,11 +7,15 @@ import com.almasb.fxgl.entity.Entity;
 import javafx.animation.Interpolator;
 import javafx.animation.TranslateTransition;
 import javafx.geometry.Point2D;
+import javafx.scene.shape.Polyline;
 import javafx.util.Duration;
 
 
 public class PointMovementSystem {
     int currentWayPoint = 0;
+    //How to get waypoints from FXGL object from TMX file
+    Polyline polyline = FXGL.getGameWorld().getEntitiesByType(map.EntityType.WAYPOINT).getFirst().getObject("polyline");
+    //polyline.getPoints returns an array of the points in the polyline
 
     public void moveEntityToX(Entity entity, double destX) {
         new AnimationBuilder()
@@ -54,5 +58,18 @@ public class PointMovementSystem {
                 moveEntityToX(entity, 50);
                 break;
         }
+//        polyline.getPoints().forEach(point -> {
+//            if (currentWayPoint == polyline.getPoints().size()-1/*-1 might need to be removed*/) {
+//                //entity reached the end of the path
+//                /*call damage to player or endgame when reach end
+//                *
+//                * or do we use a collision box instead? */
+//            }
+//            if (entity.getPosition().getX() == point) {
+//                moveEntityToY(entity, point);
+//            } else if (entity.getPosition().getY() == point) {
+//                moveEntityToX(entity, point);
+//            }
+//        });
     }
 }
--- Enemy/src/main/java/module-info.java
+++ Enemy/src/main/java/module-info.java
@@ -2,6 +2,8 @@ module Enemy {
     requires Common;
     requires com.almasb.fxgl.entity;
     requires com.almasb.fxgl.all;
+    requires Map;
     exports enemy.data;
+    exports enemy.services;
     //requires Map;
 }
