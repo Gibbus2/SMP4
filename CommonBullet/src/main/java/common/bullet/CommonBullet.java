91252fdc9505b233962438b392a354fba7a164bd
Oliverxll <bibern@live.dk>
2024-05-02 11:55:13 +0200

    Same as previous commit. Removed a lot of unnecessary logic.

--- CommonBullet/src/main/java/common/bullet/CommonBullet.java
+++ CommonBullet/src/main/java/common/bullet/CommonBullet.java
@@ -5,57 +5,31 @@ import com.almasb.fxgl.entity.component.Component;
 import enemy.Enemy;
 import javafx.geometry.Point2D;
 import objectPool.IObjectPool;
+import objectPool.PooledObjectComponent;
 
 public class CommonBullet extends Component {
-    private double speed;
-    private int damage;
-    private Entity target;
-    private IObjectPool objectPool;
-    private String objectPoolName;
+    protected double speed;
+    protected int damage;
+    protected Entity target;
 
-    public CommonBullet(double speed, int damage, Entity target,IObjectPool objectPool, String objectPoolName) {
-        this.speed = speed;
-        this.damage = damage;
+    public CommonBullet(Entity target) {
+        this.speed = 10;
+        this.damage = 1;
         this.target = target;
-        this.objectPool = objectPool;
-        this.objectPoolName = objectPoolName;
     }
+
     @Override
     public void onUpdate(double tpf) {
-        //rotate towards target
-        Point2D p1 = getEntity().getPosition();
-        Point2D p2 = target.getPosition();
-        double deltaX = p1.getX() - p2.getX();
-        double deltaY = p1.getY() - p2.getY();
-
-        // Calculate the angle in radians using atan2
-        double angleRad = Math.atan2(deltaY, deltaX);
-
-        // Convert the angle from radians to degrees and rotate it 180 deg
-        double angleDeg = Math.toDegrees(angleRad) - 180;
-
-        if (angleDeg < 0) {
-            angleDeg += 360;
+        if (target != null) {
+            getEntity().rotateToVector(target.getPosition().subtract(entity.getPosition()));
+            getEntity().translateTowards(target.getPosition(), speed * tpf);
         }
-
-        getEntity().setRotation(angleDeg);
-        double radians = Math.toRadians(getEntity().getRotation());
-        double y = speed * Math.sin(radians) * tpf;
-        double x = speed * Math.cos(radians) * tpf;
-        getEntity().setPosition(getEntity().getPosition().add(x,y));
-
-    }
-    public void returnToObjectPool() {
-        objectPool.getPool(objectPoolName).returnEntityToPool(getEntity());
     }
 
-    public double getSpeed() {
-        return speed;
-    }
-    public double getDamage() {
+    public int getDamage() {
         return damage;
     }
-    public Entity getTarget() {
-        return target;
+    public void setTarget(Entity target) {
+        this.target = target;
     }
 }
